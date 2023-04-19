4fe6d43970d7a6f167b59116c1e3cbad65633a46
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-04-20 02:59:19 +0530

    quick sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,6 +14,32 @@
 
 function quickSort(nums) {
   // code goes here
+
+  // base case, arr of length 1 or 0 are already sorted
+  if (nums.length <= 1) return nums;
+
+  // last num is pivot
+  const pivot = nums[nums.length - 1];
+
+  const left = [];
+
+  const right = [];
+
+  // sort all numbers less than pivot to left arr and the ones bigger than pivot to right
+  for (let index = 0; index < nums.length - 1; index++) {
+    if (nums[index] < pivot) {
+      left.push(nums[index]);
+    } else {
+      right.push(nums[index]);
+    }
+  }
+
+  // call quick sort on left and right
+  // concat all into one big array with pivot in the middle
+  return [...quickSort(left), pivot, ...quickSort(right)];
+
+  // the above is equivalent to
+  // return quickSort(left).concat(pivot, quickSort(right))
 }
 
 // unit tests
