a5381f4f4bc3d3784d6a37bb10901078491cc394
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-04-19 10:55:50 +0530

    merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -8,6 +8,31 @@
 
 const mergeSort = (nums) => {
   // code goes here
+
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  const length = nums.length;
+  const middle = Math.floor(length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+  const results = [];
+
+  while (left.left && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  results.concat(left, right);
 };
 
 // unit tests
