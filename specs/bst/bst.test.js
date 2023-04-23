0592530951c45da6e027f83418028f4d770019a3
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-04-23 18:33:15 +0530

    binary search tree

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -18,12 +18,66 @@ right - Node/object - the right node which itself may be another tree
 
 class Tree {
   // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+
+      while (true) {
+        // go left
+        if (current.value > value) {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          // go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    return this;
+  }
+
+  toJSON() {
+    return JSON.stringify(this.root.serialize(), null, 4);
+  }
+
+  toObject() {
+    return this.root.serialize();
+  }
 }
 
 // you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  // code maybe goes here
+  constructor(value = null, left = null, right = null) {
+    this.left = left;
+    this.right = right;
+    this.value = value;
+  }
+
+  serialize() {
+    const ans = { value: this.value };
+
+    ans.left = this.left === null ? null : this.left.serialize();
+    ans.right = this.right === null ? null : this.right.serialize();
+
+    return ans;
+  }
+}
 
 // unit tests
 // do not modify the below code
